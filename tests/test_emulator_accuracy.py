b9ed11f041d73f16e45868dac26d13ee635ff5d8
Nathan Musoke <nathan.musoke@gmail.com>
2020-03-05 17:13:59 +1300

    tests: Add tests of emulating simple functions
    These don't include other basic tests such as whether the true function
    or emulated function is evaluated; what happens when a value far outside
    the training data is calculated.
    
    Scalar inputs currently fail to run.

---
+++ tests/test_emulator_accuracy.py
@@ -0,0 +1,93 @@
+import numpy as np
+import pytest
+
+from learn_as_you_go.emulator import emulator
+
+DEFAULT_ABS_ERR = 0.05
+DEFAULT_REL_ERR = 1.0
+
+
+@pytest.mark.parametrize("xdim", [pytest.param(1, marks=pytest.mark.xfail), 2, 3])
+def test_constant(xdim):
+    """
+    Test that a constant function can be emulated
+
+    TODO: test that the emulatted function is called, not the true function.
+    """
+
+    CONSTANT = 1.0
+
+    @emulator
+    def constant(x):
+        return CONSTANT
+
+    for x in [np.random.uniform(size=xdim) for _ in range(1000)]:
+        constant(x)
+
+    assert constant.trained
+
+    x = 0.5 * np.ones(xdim)
+
+    assert np.isclose(constant(x), CONSTANT)
+
+
+@pytest.mark.parametrize("xdim", [pytest.param(1, marks=pytest.mark.xfail), 2, 3])
+def test_dot(xdim):
+    """
+    Test that a simple non-constant function can be emulated
+
+    TODO: test that the emulated function is called, not the true function.
+    """
+
+    def true_function(x):
+        return np.dot(x, x)
+
+    @emulator
+    def emulated_function(x):
+        return true_function(x)
+
+    np.random.seed(0)
+    for x in [np.random.uniform(size=xdim) for _ in range(1000)]:
+        emulated_function(x)
+
+    assert emulated_function.trained
+
+    x = 0.5 * np.ones(xdim)
+
+    true_val = true_function(x)
+    emul_val = emulated_function(x)
+
+    assert np.abs(true_val - emul_val) < DEFAULT_ABS_ERR
+    assert np.abs((emul_val - true_val) / true_val) < DEFAULT_REL_ERR
+
+
+@pytest.mark.parametrize("xdim", [pytest.param(1, marks=pytest.mark.xfail), 2, 3])
+def test_sin(xdim):
+    """
+    Test that a simple non-constant function can be emulated
+
+    This is a sinusoid in the first dimension, constant in others.
+
+    TODO: test that the emulated function is called, not the true function.
+    """
+
+    def true_function(x):
+        return np.sin(10 * x[0])
+
+    @emulator
+    def emulated_function(x):
+        return true_function(x)
+
+    np.random.seed(0)
+    for x in [np.random.uniform(size=xdim) for _ in range(1000)]:
+        emulated_function(x)
+
+    assert emulated_function.trained
+
+    x = 0.5 * np.ones(xdim)
+
+    true_val = true_function(x)
+    emul_val = emulated_function(x)
+
+    assert np.abs(true_val - emul_val) < DEFAULT_ABS_ERR
+    assert np.abs((emul_val - true_val) / true_val) < DEFAULT_REL_ERR
