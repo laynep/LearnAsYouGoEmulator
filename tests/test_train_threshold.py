c7d6f49e7330338e6e897454a151911209ffc4ba
Nathan Musoke <nathan.musoke@gmail.com>
2020-03-05 16:02:48 +1300

    test: Check that first training is when expected
    These tests fail at the moment but should pass by the time I am done.

---
+++ tests/test_train_threshold.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pytest
+
+from learn_as_you_go.emulator import emulator
+
+
+@pytest.mark.xfail
+def test_default_initial_training_threshold():
+    """
+    Test that the default initial training threshold of 1000 is used
+
+    The emulator should remain in an untrained state for the first 1000 calls,
+    then train for the first time.
+    """
+
+    xdim = 2
+    CONSTANT = 1.0
+    training_threshold = 1000
+
+    @emulator
+    def constant(x):
+        return CONSTANT
+
+    for x in [np.random.uniform(size=xdim) for _ in range(training_threshold)]:
+        assert not constant.trained
+        constant(x)
+
+    assert constant.trained
+
+
+@pytest.mark.xfail
+def test_custom_initial_training_threshold():
+    """
+    Test that the initial training threshold can be chosen
+
+    The emulator should remain in an untrained state for the first N calls,
+    then train for the first time.
+    """
+
+    xdim = 2
+    CONSTANT = 1.0
+    training_threshold = 1000
+    retraining_threshold = 1000
+
+    @emulator
+    def constant(x):
+        return CONSTANT
+
+    emulator.overrideDefaults(training_threshold, retraining_threshold)
+
+    for x in [np.random.uniform(size=xdim) for _ in range(training_threshold)]:
+        assert not constant.trained
+        constant(x)
+
+    assert constant.trained
